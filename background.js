d2328aebe1cfc665604b5c20dabb4013ebcad33e
Topaz Tee <topaztee@gmail.com>
2019-04-07 00:11:57 +1100

    working rainbow checklist
    background script sends event on url change, content has a listener.

--- background.js
+++ background.js
@@ -1,3 +1,13 @@
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-  chrome.tabs.sendMessage(integer tabId, any message, object options, function responseCallback)
+chrome.webNavigation.onHistoryStateUpdated.addListener(function(tabs) {
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    if (tabs.length) {
+      if (tabs[0].url.includes("trello.com/c")) {
+        chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function (response) {
+          if (response !== null) {
+            console.log(response);
+          }
+        });
+      }
+    }
+  });
 });
--- content.js
+++ content.js
@@ -1,17 +1,16 @@
-// var realPushState = history.pushState;
-// history.pushState = function() {
-//   if (document.getElementsByClassName('checklist-progress-bar-current js-checklist-progress-bar').length) {
-//     checklistElem = document.getElementsByClassName('checklist-progress-bar-current js-checklist-progress-bar')
-//     checklistElem[0].style["background"] = 'linear-gradient(to right, #b827fc 0%, #2c90fc 25%, #b8fd33 50%, #fec837 75%, #fd1892 100%)';
-//   }
-//
-//   return realPushState.apply(history, arguments); // leave this line exactly as-is.
-// };
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  // console.log("a request to start listending for click events");
+  // console.info('contentscript', request, sender);
+  //   console.log(sender.tab ?
+  //     "from a content script:" + sender.tab.url :
+  //     "from the extension");
+    if (request.greeting == "hello") {
+      sendResponse({farewell: "goodbye"});
+      if (document.getElementsByClassName('checklist-progress-bar-current js-checklist-progress-bar').length) {
+        checklistElem = document.getElementsByClassName('checklist-progress-bar-current js-checklist-progress-bar');
+        checklistElem[0].style["background"] = 'linear-gradient(to right, #b827fc 0%, #2c90fc 25%, #b8fd33 50%, #fec837 75%, #fd1892 100%)';
+      }
+    }
+});
 
-  // console.log(chrome.tabs);
-// chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-// });
-
-console.log('hello');
-console.log(window.location.pathname);
 
